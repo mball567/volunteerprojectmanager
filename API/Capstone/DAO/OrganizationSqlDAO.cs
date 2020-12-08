ec919e432e89bfdff7bd79fda402c5c81481b044
Michael Ball <mball567@gmail.com>
2020-12-08 12:49:09 -0500

    Added SqlDAOs

---
+++ API/Capstone/DAO/OrganizationSqlDAO.cs
@@ -0,0 +1,95 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO
+{
+    public class OrganizationSqlDAO
+    {
+        private readonly string connectionString;
+
+        public OrganizationSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public bool CreateOrganization(Organization org)
+        {
+            string sql = @"Insert into organizations (user_id, org_name, org_image, org_bio, org_zipcode, org_city, org_state, org_contact_email)
+                           VALUES (@userID, @orgName, @orgImg, @orgBio, @orgZipCode, @orgCity, @orgState, @orgContactEmail)";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userID", org.userId);
+                    cmd.Parameters.AddWithValue("@orgName", org.orgName);
+                    cmd.Parameters.AddWithValue("@profImg", org.orgImage);
+                    cmd.Parameters.AddWithValue("@orgBio", org.orgBio);
+                    cmd.Parameters.AddWithValue("@orgZipCode", org.orgZip);
+                    cmd.Parameters.AddWithValue("@orgCity", org.orgCity);
+                    cmd.Parameters.AddWithValue("@orgState", org.orgState);
+                    cmd.Parameters.AddWithValue("@orgContactEmail", org.orgContactEmail);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
+
+        public Organization getOrganizationOnLogin(int userID)
+        {
+            string sql = @"Select * from organizations where user_id = @userID";
+            Organization org = new Organization();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userID", userID);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        org.userId = Convert.ToInt32(rdr["user_id"]);
+                        org.orgName = Convert.ToString(rdr["org_name"]);
+                        org.orgImage = Convert.ToString(rdr["org_image"]);
+                        org.orgBio = Convert.ToString(rdr["org_bio"]);
+                        org.orgZip = Convert.ToInt32(rdr["org_zipcode"]);
+                        org.orgCity = Convert.ToString(rdr["org_city"]);
+                        org.orgState = Convert.ToString(rdr["org_state"]);
+                        org.orgContactEmail = Convert.ToString(rdr["org_contact_email"]);
+                        org.orgId = Convert.ToInt32(rdr["org_id"]);
+                    }
+                    return org;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ API/Capstone/DAO/ProfileSqlDAO.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Capstone.Models;
+using Capstone.Security;
+using Capstone.Security.Models;
+
+namespace Capstone.DAO
+{
+    public class ProfileSqlDAO
+    {
+        private readonly string connectionString;
+
+        public ProfileSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public bool CreateProfile(Profile profile)
+        {
+            string sql = @"Insert into profiles (user_id, first_name, last_name, prof_image, bio, prof_zipcode, prof_city, prof_state, prof_contact_email)
+                           VALUES (@userID, @firstName, @lastName, @profImg, @bio, @profZipCode, @profCity, @profState, @profContactEmail)";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userID", profile.userId);
+                    cmd.Parameters.AddWithValue("@firstName", profile.firstName);
+                    cmd.Parameters.AddWithValue("@lastName", profile.lastName);
+                    cmd.Parameters.AddWithValue("@profImg", profile.profileImage);
+                    cmd.Parameters.AddWithValue("@bio", profile.bio);
+                    cmd.Parameters.AddWithValue("@profZipCode", profile.profZip);
+                    cmd.Parameters.AddWithValue("@profCity", profile.profCity);
+                    cmd.Parameters.AddWithValue("@profState", profile.profState);
+                    cmd.Parameters.AddWithValue("@profContactEmail", profile.profContactEmail);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
+
+        public Profile getProfileOnLogin(int userID)
+        {
+            string sql = @"Select * from profiles where user_id = @userID";
+            Profile profile = new Profile();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@userID", userID);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        profile.userId = Convert.ToInt32(rdr["user_id"]);
+                        profile.firstName = Convert.ToString(rdr["first_name"]);
+                        profile.lastName = Convert.ToString(rdr["last_name"]);
+                        profile.profileImage = Convert.ToString(rdr["prof_image"]);
+                        profile.bio = Convert.ToString(rdr["bio"]);
+                        profile.profZip = Convert.ToInt32(rdr["prof_zipcode"]);
+                        profile.profCity = Convert.ToString(rdr["prof_city"]);
+                        profile.profState = Convert.ToString(rdr["prof_state"]);
+                        profile.profContactEmail = Convert.ToString(rdr["prof_contact_email"]);
+                        profile.profileId = Convert.ToInt32(rdr["profile_id"]);
+                    }
+                    return profile;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
+    }
+}
