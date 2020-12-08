d9e14222b5480afa0c13be1ef6e2a6d297e4494a
Brian Moody <bkmoody61@gmail.com>
2020-12-08 10:40:48 -0500

    SQL Database Update

--- API/database/capstone.sql
+++ API/database/capstone.sql
@@ -29,11 +29,12 @@ CREATE TABLE profiles (
 	user_id int NOT NULL,
 	first_name varchar(100) NOT NULL,
 	last_name varchar(100) NOT NULL,
+	prof_image varchar(200) NOT NULL,
 	bio varchar(1000) NOT NULL,
-	prof_location_zipcode int NOT NULL,
-	prof_location_city varchar (100) NOT NULL,
-	prof_location_state varchar (50) NOT NULL,
-	contact_email varchar (200) NOT NULL,
+	prof_zipcode int NOT NULL,
+	prof_city varchar (100) NOT NULL,
+	prof_state varchar (50) NOT NULL,
+	prof_contact_email varchar (200) NOT NULL,
 	CONSTRAINT PK_profiles PRIMARY KEY (profile_id),
 	CONSTRAINT FK_profiles FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
@@ -42,11 +43,12 @@ CREATE TABLE organizations(
 	org_id int IDENTITY(1,1) NOT NULL,
 	user_id int NOT NULL,
 	org_name varchar (100) NOT NULL,
+	org_image varchar (200) NOT NULL,
 	org_bio varchar (1000) NOT NULL,
-	org_location_zipcode int NOT NULL,
-	org_location_city varchar (100) NOT NULL,
-	org_location_state varchar (50) NOT NULL,
-	contact_email varchar (200) NOT NULL,
+	org_zipcode int NOT NULL,
+	org_city varchar (100) NOT NULL,
+	org_state varchar (50) NOT NULL,
+	org_contact_email varchar (200) NOT NULL,
 	CONSTRAINT PK_organizations PRIMARY KEY (org_id),
 	CONSTRAINT FK_organizations_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
@@ -56,11 +58,12 @@ CREATE TABLE projects (
 	org_id int NULL,
 	proj_name varchar(100) NOT NULL,
 	proj_desc varchar(1000) NOT NULL,
-	proj_location_zipcode int NOT NULL,
-	proj_location_city varchar (100) NOT NULL,
-	proj_location_state varchar (50) NOT NULL,
+	proj_image varchar (200) NOT NULL,
+	proj_zipcode int NOT NULL,
+	proj_city varchar (100) NOT NULL,
+	proj_state varchar (50) NOT NULL,
 	proj_working_hours int NOT NULL,
-	point_of_contact int NOT NULL,
+	proj_contact_email varchar (200) NOT NULL,
 	CONSTRAINT PK_projects PRIMARY KEY (proj_id),
 	CONSTRAINT FK_projects_org_id FOREIGN KEY (org_id) REFERENCES organizations (org_id)
 );
@@ -69,10 +72,11 @@ CREATE TABLE teams(
 	team_id int IDENTITY(1,1) NOT NULL,
 	team_name varchar (100) NOT NULL,
 	team_bio varchar (1000) NOT NULL,
-	team_location_zipcode int NOT NULL,
-	team_location_city varchar (100) NOT NULL,
-	team_location_state varchar (50) NOT NULL,
-	team_contact int NOT NULL,
+	team_image varchar (200) NOT NULL,
+	team_zipcode int NOT NULL,
+	team_city varchar (100) NOT NULL,
+	team_state varchar (50) NOT NULL,
+	team_contact_email varchar (200) NOT NULL,
 	CONSTRAINT PK_teams PRIMARY KEY (team_id),
 );
 
@@ -119,11 +123,41 @@ CREATE TABLE teams_causes (
 	CONSTRAINT pk_teams_causes_teams_causes PRIMARY KEY (team_id, cause_id),
 	CONSTRAINT fk_teams_causes_team_id FOREIGN KEY (team_id) REFERENCES teams (team_id),
 	CONSTRAINT fk_teams_causes_cause_id FOREIGN KEY (cause_id) REFERENCES causes (cause_id)
+);
+
+CREATE TABLE projects_causes (
+	proj_id int NOT NULL,
+	cause_id int NOT NULL,
+	CONSTRAINT pk_projects_causes_projects_causes PRIMARY KEY (proj_id, cause_id),
+	CONSTRAINT fk_projects_causes_proj_id FOREIGN KEY (proj_id) REFERENCES projects (proj_id),
+	CONSTRAINT fk_projects_causes_cause_id FOREIGN KEY (cause_id) REFERENCES causes (cause_id)
 )
 
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+INSERT INTO causes (cause_name) VALUES ('Animals');
+INSERT INTO causes (cause_name) VALUES ('Arts and Culture');
+INSERT INTO causes (cause_name) VALUES ('Children');
+INSERT INTO causes (cause_name) VALUES ('Community');
+INSERT INTO causes (cause_name) VALUES ('Crisis Support');
+INSERT INTO causes (cause_name) VALUES ('Disaster Relief');
+INSERT INTO causes (cause_name) VALUES ('Education and Literacy');
+INSERT INTO causes (cause_name) VALUES ('Emergency');
+INSERT INTO causes (cause_name) VALUES ('Environment');
+INSERT INTO causes (cause_name) VALUES ('Faith Based');
+INSERT INTO causes (cause_name) VALUES ('Family Support');
+INSERT INTO causes (cause_name) VALUES ('Health and Medicine');
+INSERT INTO causes (cause_name) VALUES ('Housing');
+INSERT INTO causes (cause_name) VALUES ('Hunger');
+INSERT INTO causes (cause_name) VALUES ('Legal');
+INSERT INTO causes (cause_name) VALUES ('Mental Health');
+INSERT INTO causes (cause_name) VALUES ('Seniors');
+INSERT INTO causes (cause_name) VALUES ('Special Needs');
+INSERT INTO causes (cause_name) VALUES ('Sports and Recreation');
+INSERT INTO causes (cause_name) VALUES ('Veterans');
+INSERT INTO causes (cause_name) VALUES ('Women');
 
 GO
