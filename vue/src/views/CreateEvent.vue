4a59ffb2d575bdb164c0237d755cd2e8289ce299
David Felson <davidfelson@gmail.com>
2020-12-17 17:52:08 -0500

    Updated CSS for CreateEvent view

--- vue/src/views/CreateEvent.vue
+++ vue/src/views/CreateEvent.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="create-event">
-    <h1>Create Event</h1>
+  <div id="create-event" class="container"> 
+    <div id="createForm">
+      <h1>Create Event</h1>
       <form class="form-createevent" @submit.prevent="createEvent">
-      <label id="eventName" for="eventName" class="sr-only">Event Name: </label>
+      <!-- <label id="eventName" for="eventName" class="sr-only">Event Name: </label> -->
       <input
         type="text"
         id="eventName"
@@ -13,11 +14,11 @@
         autofocus
       /> <br/>
 
-      <label>Drag and drop your Event image here:</label>
+      <!-- <label>Drag and drop your Event image here:</label> -->
       <drop-zone id="dropzone"/>
 
       <div class="eventDescription">
-      <label for="eventDesc" class="sr-only">Event Description: </label> <br/>
+      <!-- <label for="eventDesc" class="sr-only">Event Description: </label> <br/> -->
       <textarea
         type="text"
         id="eventDesc"
@@ -30,32 +31,32 @@
       />
       </div>
 
-      <label for="eventDate">Event Date: </label>
+      <!-- <label for="eventDate">Event Date: </label> -->
       <input id="eventDate" type="date" v-model="event.eventDate" required />
       <br />
       
-      <label for="eventStartTime">Event Start Time: </label>
+      <!-- <label for="eventStartTime">Event Start Time: </label> -->
       <input id="eventStartTime" type="time" v-model="event.eventStartTime" required />
       <br />
 
             
-      <label for="eventEndTime">Event End Time: </label>
+      <!-- <label for="eventEndTime">Event End Time: </label> -->
       <input id="eventEndTime" type="time" v-model="event.eventEndTime" required />
       <br />
 
-      <label for="eventHours">Estimated hours for event completion: </label>
+      <!-- <label for="eventHours">Estimated hours for event completion: </label> -->
       <input id="eventHours" type="number" placeholder="Hours To Complete" v-model="event.eventWorkingHours" required />
       <br />
       
-      <label for="eventCity">City: </label>
+      <!-- <label for="eventCity">City: </label> -->
       <input id="eventCity" type="text" placeholder="City" v-model="event.EventCity" required />
       <br />
 
-      <label for="eventState">State: </label>
+      <!-- <label for="eventState">State: </label> -->
       <input id="evnetState" type="text" placeholder="State" v-model="event.EventState" required />
       <br />
 
-      <label for="eventZip" class="sr-only">Zip Code: </label>
+      <!-- <label for="eventZip" class="sr-only">Zip Code: </label> -->
       <input
         type="text"
         id="eventZip"
@@ -66,13 +67,16 @@
       />
       <br />
 
-      <label for="e-mail">Lead Contact E-Mail: </label>
+      <!-- <label for="e-mail">Lead Contact E-Mail: </label> -->
       <input id="e-mail" type="email" placeholder="E-Mail Address" v-model="event.EventContactEmail" required /> <br />
 
+      <div id="buttonDiv">
       <button class="btn btn-lg btn-primary btn-block" type="submit">
         Create Event
       </button>
+      </div>
     </form>
+  </div>
   </div>
 </template>
 
@@ -136,22 +140,57 @@ export default {
 
 <style scoped>
 
+div#create-event.container{
+  display: grid;
+  grid-template-columns: 375px 1fr 375px;
+  grid-template-areas: 
+  ". form .";
+}
+
+@media screen and (max-width: 1024px){
+div#create-event.container{
+  grid-template-columns: 200px 1fr 200px;
+}
+}
+
+@media screen and (max-width: 768px){
+div#create-event.container{
+  grid-template-columns: 100px 1fr 100px;
+}
+}
+
+div#createForm{
+  grid-area: form;
+}
+
 div#create-event h1{
   color: white;
   margin-block: 0.5em;
   padding-left: 0.75em;
+  text-align: center;
 }
 
 div#create-event{
   display: block;
   text-align: left;
 }
+input{
+  width: 100%;
+  height: 40px;
+  border-radius: 4px; 
+  background-color: #fff; 
+  box-shadow: 1px 2px 5px rgba(0,0,0,.5); 
+  border: solid 1px #cbc9c9;
+  margin-top: 0.75em; 
+}
 
-form{
-  display: inline-block;
-  margin-left: auto;
-  margin-right: auto;
-  text-align: left;
+
+input,
+textarea,
+select{
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  color: #4c4c4c;
 }
 
 form label {
@@ -160,27 +199,21 @@ form label {
   padding-right: 0.5em;
 }
 
-form input#eventName {
-  margin-top: 2.25em;
-}
-
 #dropzone{
-  margin-left: 1.5em;
-  margin-block: 0.5em;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  color: #4c4c4c;
+  width: 100%;
+  margin-top: 0.75em;
+  border-radius: 4px;
 }
 
 textarea#eventDesc{
-  margin-bottom: 0.5em;
-}
-
-form input#e-mail {
-  margin-bottom: 1.25em;
-}
-
-form input {
-  padding-top: 0.25em;
-  padding-bottom: 0.25em;
-  margin: 0.25em;
+  border-radius: 4px;
+  width: 100%;
+  margin-top: 0.75em;
+  margin-bottom: 0em;
+  margin-left: -.2em;
 }
 
 form textarea{
@@ -188,10 +221,6 @@ form textarea{
   padding-bottom: 0.25em;
   margin-top: 0.5em;
   margin-left: 1.75em;
-}
-
-.eventDescription * {
-  vertical-align: top;
 }
 
 div#user-profile form #router-link{
@@ -203,15 +232,15 @@ div#user-profile form #router-link{
 div#create-event form button {
   margin-top: 1.5em;
   padding: 0.5em;
-  margin-left: 1.5em;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center
 }
 
-p#causeSelect{
-  padding-left: 0.2em;
+#buttonDiv{
+  width: 100%;
+  text-align: center;
 }
 
-form div#causeList {
-  color: white;
-  padding-left: 1.25em;
-}
+
 </style>
